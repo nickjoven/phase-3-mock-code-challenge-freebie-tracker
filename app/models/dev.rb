e417c1dc3631c0c202a37c30bc76dc65b8968d4f
Nicholas <nicholasjoven@gmail.com>
2022-08-17 11:34:12 -0400

    completed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,19 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    def received_one?(item_name)
+        puts "Does #{self.name} own #{item_name}..?"
+        self.freebies.pluck(:item_name).include? item_name
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            "#{self.name} gave #{dev.name} their #{freebie.item_name}."
+            freebie.update!(dev_id: dev.id)
+        else
+            "#{self.name} can't give away someone else's #{freebie.item_name}!"
+        end
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+puts "Destroying previous seeds..."
+
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -16,6 +22,12 @@ puts "Creating freebies..."
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
-# Create freebies Here
+
+Freebie.create(item_name: "Water Bottle", value: 10, dev_id: 1, company_id: 1,)
+Freebie.create(item_name: "Hat", value: 12, dev_id: 2, company_id: 1,)
+Freebie.create(item_name: "Lanyard", value: 3, dev_id: 3, company_id: 2,)
+Freebie.create(item_name: "Sweater", value: 40, dev_id: 3, company_id: 3,)
+Freebie.create(item_name: "Beanie", value: 8, dev_id: 2, company_id: 4,)
+Freebie.create(item_name: "Mousepad", value: 9, dev_id: 1, company_id: 3,)
 
 puts "Seeding done!"
